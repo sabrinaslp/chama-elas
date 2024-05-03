654fccae2dc73845682fdc265eae09fc0c220b76
Sabrina <sabrinaslimap@gmail.com>
2024-05-02 22:40:22 -0300

    Adicionado outros arquivos complementares para estrutura inicial

---
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/ChamaElasApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.chamaelas.ChamaElas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaElasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamaElasApplication.class, args);
+	}
+
+}
---
+++ ChamaElas/src/test/java/com/soulcode/chamaelas/ChamaElas/ChamaElasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.chamaelas.ChamaElas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaElasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
