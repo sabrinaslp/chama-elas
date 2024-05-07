9a48411b1a6e6f1c20fd51f13523147b5807d0e9
LarissaLisboa <larissa-lisboa99@hotmail.com>
2024-05-07 19:47:54 -0300

    Implementação métodos controllers 2

--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/UsuarioRepository.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/UsuarioRepository.java
@@ -4,7 +4,9 @@ import com.soulcode.chamaelas.ChamaElas.models.UsuarioModel;
 import com.soulcode.chamaelas.ChamaElas.models.dto.UsuarioDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-    UsuarioDTO findByEmail(String email);
+    Optional<UsuarioModel> findByEmail(String email);
 
 }
