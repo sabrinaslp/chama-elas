28077d4c085e3d8cdc24562247f26b1e163140d7
LarissaLisboa <larissa-lisboa99@hotmail.com>
2024-05-07 19:50:44 -0300

    Implementação métodos controllers 3

--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/ClienteRepository.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.soulcode.chamaelas.ChamaElas.repositories;
 
 import com.soulcode.chamaelas.ChamaElas.models.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
 }
--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/FuncaoRepository.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/repositories/FuncaoRepository.java
@@ -2,6 +2,8 @@ package com.soulcode.chamaelas.ChamaElas.repositories;
 
 import com.soulcode.chamaelas.ChamaElas.models.FuncaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncaoRepository extends JpaRepository<FuncaoModel,Long> {
 }
