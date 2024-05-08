4502361c5c340db381492db25abf435f70db07de
Sabrina <sabrinaslimap@gmail.com>
2024-05-08 16:12:52 -0300

    Melhoria no método para obter função do usuário no cadastro

--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/services/UsuarioService.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/services/UsuarioService.java
@@ -32,40 +32,27 @@ public class UsuarioService {
 
     public void cadastrarNovoUsuario(String nome, String email, String senha, String confirmacaoSenha, String funcao, Model model) {
         autenticacaoService.verificarCadastroUsuario(nome, email, senha, confirmacaoSenha);
-        var funcaoModel = obterOuCriarFuncao(funcao);
-        var usuarioModel = criarUsuario(nome, email, senha, funcaoModel);
+        var funcaoNovoUsuario = atribuirFuncaoAoUsuario(funcao);
+        var usuarioModel = criarUsuario(nome, email, senha, funcaoNovoUsuario);
         usuarioRepository.save(usuarioModel);
 
         model.addAttribute("successMessage", "Usuário cadastrado com sucesso! Faça o login para acessar sua conta.");
     }
 
-    private FuncaoModel obterOuCriarFuncao(String nomeFuncao) {
-        FuncaoModel funcaoModel;
-        switch (nomeFuncao) {
-            case "Cliente":
-                funcaoModel = funcaoRepository.findByNome(FuncaoModel.Values.CLIENTE.getNome());
-                if (funcaoModel == null) {
-                    funcaoModel = funcaoRepository.save(new FuncaoModel(2L, "Cliente"));
-                }
-                break;
-            case "Tecnico":
-                funcaoModel = funcaoRepository.findByNome(FuncaoModel.Values.TECNICO.getNome());
-                if (funcaoModel == null) {
-                    funcaoModel = funcaoRepository.save(new FuncaoModel(1L, "Técnico"));
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("Tipo de usuário inválido: " + nomeFuncao);
-        }
-        return funcaoModel;
+    private FuncaoModel atribuirFuncaoAoUsuario(String funcao) {
+        return switch (funcao) {
+            case "Cliente" -> funcaoRepository.findByNome(FuncaoModel.Values.CLIENTE.getNome());
+            case "Tecnico" -> funcaoRepository.findByNome(FuncaoModel.Values.TECNICO.getNome());
+            default -> throw new IllegalArgumentException("Tipo de usuário inválido: " + funcao);
+        };
     }
 
-    private UsuarioModel criarUsuario(String nome, String email, String senha, FuncaoModel funcaoModel) {
+    private UsuarioModel criarUsuario(String nome, String email, String senha, FuncaoModel funcaoNovoUsuario) {
         UsuarioModel usuarioModel = new UsuarioModel();
         usuarioModel.setNome(nome);
         usuarioModel.setEmail(email);
         usuarioModel.setSenha(senha);
-        usuarioModel.setFuncao(funcaoModel);
+        usuarioModel.setFuncao(funcaoNovoUsuario);
 
         return usuarioModel;
     }
