90674ae2d0a2fc92811ef5f85bafd3b15bfc9cbb
Sabrina <sabrinaslimap@gmail.com>
2024-05-08 19:32:58 -0300

    Adicionando métodos para UsuarioController

--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/controllers/thymeleaf/UsuarioController.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/controllers/thymeleaf/UsuarioController.java
@@ -1,4 +1,42 @@
 package com.soulcode.chamaelas.ChamaElas.controllers.thymeleaf;
 
+import com.soulcode.chamaelas.ChamaElas.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/cadastro-usuario")
+    public String mostrarPaginaCadastroUsuario() {
+        return "cadastro-usuario";
+    }
+
+    @PostMapping("/cadastro-usuario")
+    public String cadastrarNovoUsuario(@RequestParam("nome") String nome,
+                                       @RequestParam("email") String email,
+                                       @RequestParam("senha") String senha,
+                                       @RequestParam("confirmaSenha") String confirmacaoSenha,
+                                       @RequestParam("funcao") String funcao,
+                                       Model model) {
+        try {
+            usuarioService.cadastrarNovoUsuario(nome, email, senha, confirmacaoSenha, funcao, model);
+            return "login-usuario";
+        } catch (DataIntegrityViolationException | IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "cadastro-usuario";
+        } catch (Exception e) {
+            model.addAttribute("error", "Ocorreu um erro ao processar o cadastro do usuário.");
+            return "cadastro-usuario";
+        }
+    }
+
 }
--- ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/services/UsuarioService.java
+++ ChamaElas/src/main/java/com/soulcode/chamaelas/ChamaElas/services/UsuarioService.java
@@ -1,15 +1,18 @@
 package com.soulcode.chamaelas.ChamaElas.services;
 import com.soulcode.chamaelas.ChamaElas.models.ChamadoModel;
 import com.soulcode.chamaelas.ChamaElas.models.ClienteModel;
+import com.soulcode.chamaelas.ChamaElas.models.FuncaoModel;
 import com.soulcode.chamaelas.ChamaElas.models.UsuarioModel;
 import com.soulcode.chamaelas.ChamaElas.models.dto.UsuarioDTO;
 import com.soulcode.chamaelas.ChamaElas.repositories.ChamadoRepository;
+import com.soulcode.chamaelas.ChamaElas.repositories.FuncaoRepository;
 import com.soulcode.chamaelas.ChamaElas.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -24,6 +27,12 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private FuncaoRepository funcaoRepository;
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     // Listar todos os chamados criados pelo usuário
     public List<ChamadoModel> listarChamadosUsuario(ClienteModel cliente) {
         return chamadoRepository.findByCliente(cliente);
@@ -36,6 +45,38 @@ public class UsuarioService {
     // Mostra detalhes do chamado na página do usuário (não deve alterar o status na página)
     public Optional<ChamadoModel> obterDetalhesDoChamadoPorId(Long id) {
         return chamadoRepository.findById(id);
+    }
+
+    public void cadastrarNovoUsuario(String nome, String email, String senha, String confirmacaoSenha, String funcao, Model model) {
+        autenticacaoService.verificarCadastroUsuario(nome, email, senha, confirmacaoSenha);
+        var funcaoNovoUsuario = atribuirFuncaoAoUsuario(funcao);
+        var usuarioModel = criarUsuario(nome, email, senha, funcaoNovoUsuario);
+        usuarioRepository.save(usuarioModel);
+
+        model.addAttribute("successMessage", "Usuário cadastrado com sucesso! Faça o login para acessar sua conta.");
+    }
+
+    private FuncaoModel atribuirFuncaoAoUsuario(String funcao) {
+        return switch (funcao) {
+            case "Cliente" -> funcaoRepository.findByNome(FuncaoModel.Values.CLIENTE.getNome());
+            case "Tecnico" -> funcaoRepository.findByNome(FuncaoModel.Values.TECNICO.getNome());
+            default -> throw new IllegalArgumentException("Tipo de usuário inválido: " + funcao);
+        };
+    }
+
+    private UsuarioModel criarUsuario(String nome, String email, String senha, FuncaoModel funcaoNovoUsuario) {
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setNome(nome);
+        usuarioModel.setEmail(email);
+        usuarioModel.setSenha(senha);
+        usuarioModel.setFuncao(funcaoNovoUsuario);
+
+        return usuarioModel;
+    }
+
+    // Listar todos os chamados criados pelo usuário
+    public List<ChamadoModel> listarTodosOsChamadosDoUsuario(ClienteModel usuario) {
+        return chamadoRepository.findByCliente(usuario);
     }
 
     public UsuarioDTO UsuarioPorEmail(String email){
